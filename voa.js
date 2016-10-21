9fcc117ae44cea7f1e0151e0a7c99c5aaa788b09
oetscweb <thomas.n.kirkpatrick.ctr@mail.mil>
2016-10-21 11:10:28 -0400

    Add files via upload

---
+++ voa.js
@@ -0,0 +1,22 @@
+(function($) {
+	$(function() {
+
+		var	$window = $(window),
+			$modal = $('.modalbg'),
+			$close = $('.close'),
+			$showModal = $('.showAnimation');
+
+		$close.click(function(e) {
+			$modal.hide();
+			$modal.children(".dialog").css("opacity", "0");
+			$modal.children(".dialog").css("top", "-1000px");
+		});
+
+		$showModal.click(function() {
+			var attr = $(this).attr("data-modal");
+			$("#"+attr).show();
+			$("#"+attr).children(".dialog").animate({opacity: 1, top:"-20px"},'slow');
+		});
+	});
+
+})(jQuery);
